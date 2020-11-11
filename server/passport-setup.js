2c05ff7b302560e61d1b17874546ee0b253b053a
tanbir99 <tanbir.islam@baruchmail.cuny.edu>
2020-11-11 17:02:33 -0500

    added passport deserialize for logging out

--- server/passport-setup.js
+++ server/passport-setup.js
@@ -5,11 +5,11 @@ passport.serializeUser(function(user, done) {
     done(null, user.id);
   });
   
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
-    });
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
   });
+});
 
 passport.use(new googleStrategy ({
     clientID: process.env.googleID,
